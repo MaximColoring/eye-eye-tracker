484d535ce4bad19aa04c5b1542f8fa85b101b2d2
mikolajspeichert <mikolaj.speichert@gmail.com>
2019-07-13 21:03:33 +0200

    fix bundle

--- build/index.d.ts
+++ build/index.d.ts
@@ -1,7 +1,66 @@
-import { Model } from "./models";
-export declare const Tracker: ({ searchWindow, scoreThreshold, stopOnConvergence, sharpenResponse, maxIterationsPerAnimFrame, weightPoints }: TrackerParams) => {
-    init: (pmodel?: Model) => void;
-    start: (element: HTMLVideoElement, box: number[]) => false | undefined;
-    stop: () => void;
-    getCurrentPosition: () => false | number[][];
+type mSize = [number, number]
+type Model = {
+  scoring: {
+    size: mSize
+    bias: number;
+    coef: number[];
+  };
+  path: {
+    normal: number[][];
+    vertices: number[][];
+  };
+  patchModel: {
+    patchType: "SVM" | "MOSSE";
+    bias: {
+      raw: number[];
+      sobel: number[];
+      lbp: number[];
+    };
+    weights: {
+      raw: number[][];
+      sobel: number[][];
+      lbp: number[][];
+    };
+    numPatches: number;
+    patchSize: mSize;
+    canvasSize: mSize;
+  };
+  shapeModel: {
+    eigenVectors: number[][];
+    numEvalues: number;
+    eigenValues: number[];
+    numPtsPerSample: number;
+    nonRegularizedVectors: number[];
+    meanShape: number[][];
+  };
+  hints: {
+    rightEye: [number, number];
+    leftEye: [number, number];
+    nose: [number, number];
+  };
+};
+
+type TrackerParams = {
+  /** whether to use constant velocity model when fitting (default is true) */
+  constantVelocity?: boolean
+  /** the size of the searchwindow around each point (default is 11) */
+  searchWindow?: number
+  /** threshold for when to assume we've lost tracking (default is 0.50) */
+  scoreThreshold?: number
+  /** whether to stop tracking when the fitting has converged (default is false) */
+  stopOnConvergence?: boolean
+  /** object with parameters for facedetection : */
+
+  weightPoints?: number[]
+
+  sharpenResponse?: number
+
+  maxIterationsPerAnimFrame?: number
+}
+
+export const Tracker: ({ searchWindow, scoreThreshold, stopOnConvergence, sharpenResponse, maxIterationsPerAnimFrame, weightPoints }: TrackerParams) => {
+  init: (pmodel?: Model) => void;
+  start: (element: HTMLVideoElement, box: number[]) => false | undefined;
+  stop: () => void;
+  getCurrentPosition: () => false | number[][];
 };
--- build/index.js
+++ build/index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./tracker.production.js')
+  module.exports = require('./tracker.cjs.production.js')
 } else {
-  module.exports = require('./tracker.development.js')
+  module.exports = require('./tracker.cjs.development.js')
 }
